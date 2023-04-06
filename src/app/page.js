0034f8a1f69cff4209f562e2d2d391bc37e39133
Sarvar55 <sarvar.musazade@gmail.com>
2023-04-07 00:28:09 +0300

    ci: implement commitlint

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,17 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from './page.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+          Get started by editing & nbsp;{' '}
+          <code className={styles.code}> src / app / page.js </code>{' '}
+        </p>{' '}
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -27,10 +27,9 @@ export default function Home() {
               height={24}
               priority
             />
-          </a>
-        </div>
+          </a>{' '}
+        </div>{' '}
       </div>
-
       <div className={styles.center}>
         <Image
           className={styles.logo}
@@ -42,9 +41,8 @@ export default function Home() {
         />
         <div className={styles.thirteen}>
           <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
+        </div>{' '}
       </div>
-
       <div className={styles.grid}>
         <a
           href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -53,13 +51,12 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
+            Docs <span> - & gt; </span>{' '}
+          </h2>{' '}
           <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
+            Find in -depth information about Next.js features and API.{' '}
+          </p>{' '}
         </a>
-
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
@@ -67,11 +64,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
+            Templates <span> - & gt; </span>{' '}
+          </h2>{' '}
+          <p className={inter.className}>
+            {' '}
+            Explore the Next.js 13 playground.{' '}
+          </p>{' '}
         </a>
-
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
@@ -79,13 +78,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
+            Deploy <span> - & gt; </span>{' '}
+          </h2>{' '}
           <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.{' '}
+          </p>{' '}
+        </a>{' '}
+      </div>{' '}
     </main>
-  )
+  );
 }
